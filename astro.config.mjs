9979ba0b751d455e87fdafbf528423b6ef8d0bc8
Christine Tham <chris.tham@hellotham.com>
2023-03-17 18:01:12 +1100

    Use HTML5UP Eventually site template

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://travelens.github.io',
+    experimental: {
+        assets: true,
+      },
 });

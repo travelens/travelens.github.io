b8e046d52837496c03e3aa17f1bd5277f4ac4f7e
Christine Tham <chris.tham@hellotham.com>
2023-04-15 16:02:33 +1000

    Migrated common components to astro-travelens

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import UnoCSS from 'unocss/astro';
-import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro'
+import { defineConfig } from 'astro/config'
+import { SiteMetadata } from 'astro-travelens/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://travelens.github.io',
+  site: SiteMetadata.site,
   experimental: {
     assets: true
   },
   integrations: [UnoCSS({
     injectReset: true
   })]
-});
+})
